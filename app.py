0b805340b9f03dc6ad4e88cac1392a7cd91754ba
ramandeepp1403 <101343421+ramandeepp1403@users.noreply.github.com>
2026-05-11 13:41:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,11 @@ type_mapping = {"PAYMENT": 0, "TRANSFER": 1, "CASH_OUT": 2, "DEBIT": 3}
 type_encoded = type_mapping[type_transaction]
 
 if st.button("Predict Fraud"):
+    if amount > oldbalanceOrg:
+        st.error("Invalid transaction: insufficient balance")
+    else:
     # Create input dictionary
-    new_input = {
+        new_input = {
         "step": step,
         "type": type_encoded,
         "amount": amount,
